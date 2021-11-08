68c0f0ed193b07461501d9c7e8eac18413f2a1dc
oi <oifuwe@luc.edu>
2021-11-08 15:27:43 -0600

    add in memory database and hibernate dependencies

--- currency-exchange-service/src/main/java/com/oi/currencyexchangeservice/CurrencyExchange.java
+++ currency-exchange-service/src/main/java/com/oi/currencyexchangeservice/CurrencyExchange.java
@@ -2,20 +2,31 @@ package com.oi.currencyexchangeservice;
 
 import lombok.*;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 
+@Entity
 @NoArgsConstructor
 @RequiredArgsConstructor
 @Data
 public class CurrencyExchange {
 
   @NonNull
+  @Id
   private Long id;
+
   @NonNull
+  @Column(name = "currency_from")
   private String from;
+
   @NonNull
+  @Column(name = "currency_to")
   private String to;
+
   @NonNull
   private BigDecimal conversionMultiple;
+
   private  String environment;
 }
---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+insert into currency_exchange
+    ( id, currency_from, currency_to, conversion_multiple, environment )
+values
+    ( 10001, 'USD', 'NGN', 412.32855, '' ),
+    ( 10002, 'CAD', 'NGN', 331.39074, '' ),
+    ( 10003, 'EUR', 'NGN', 477.90499, '' ),
+    ( 10004, 'AUD', 'NGN', 306.09004, '' );
